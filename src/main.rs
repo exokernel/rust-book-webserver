b1aa0da4fb45b6c96ab31fda4b9a129724d8394d
Dustin Cannon <dustin.cannon@bigcommerce.com>
2022-09-21 09:04:36 -0400

    single threaded, no error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let ip = stream.peer_addr().unwrap().ip().to_string();
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}\r\n", ip);
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
